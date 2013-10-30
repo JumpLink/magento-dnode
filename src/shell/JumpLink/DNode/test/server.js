2faf642b3b8350f320346333e1a3ec47efe9c76e
JumpLink <jumplink@gmail.com>
2013-10-30 22:19:20 +0100

    send all events to dnode server for development

--- src/shell/JumpLink/DNode/test/server.js
+++ src/shell/JumpLink/DNode/test/server.js
@@ -1,13 +1,13 @@
 var dnode = require('dnode');
 
 var server = dnode(function (remote, conn) {
-    this.welcome_message = function (messsage, answer) {
-      console.log(messsage);
-      answer("Hello Magento, you are welcome!");
-      console.log(remote);
-      remote.product_items(null,null, function (result) {
+    this.dispatchEvent = function (eventName, args, cb) {
+      console.log(eventName);
+      console.log(args);
+      cb("thx");
+/*      remote.product_items(null,null, function (result) {
         console.log(result);
-      });
+      });*/
     };
 });
-server.listen(6060);
+server.listen(7070);
